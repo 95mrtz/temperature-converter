4de7822b37b1ff381826f839d68cae7d496368d4
manuelrtz <manuelrtzdev@gmail.com>
2022-07-08 18:43:12 -0300

    A little history...

--- src/components/mainComponents/Title.js
+++ src/components/mainComponents/Title.js
@@ -7,6 +7,13 @@ const Title = ({ type, setType }) => {
 
   return (
     <section className="flex flex-col justify-center items-center my-1 sm:my-5">
+      <p className="text-center text-xl sm:2xl">
+        <span className="font-bold">Historia:</span> <br/>
+        <span className="font-bold"> Anders Celsius </span> definió su escala en 1742 considerando las temperaturas de ebullición y de congelación del agua,
+         el grado Celsius​ (símbolo °C) reemplaza, desde 1948,​ a los grados centígrados​ en el Sistema Internacional de Unidades. <br />
+         <span className="font-bold">Daniel Gabriel Fahrenheit</span> ecidió crear una escala de temperatura basada en tres puntos de temperatura fijos: la del agua congelada, la temperatura del cuerpo humano y el punto más frío en el que podría enfriar una y otra vez la solución de agua, hielo Y una especie de sal, el cloruro de amonio.  
+         Esta escala se utilizaba en la mayoría de los países anglosajones y Puerto Rico para todo tipo de uso. 
+      </p>
       <h1 className="text-2xl sm:text-3xl md:text-4xl	mb-2">
         {" "}
         {type === "celsius"
